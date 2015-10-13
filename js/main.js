85144bdf53fd6bf31910eae2f3593a7672f1c689
winrox <winnie.mabee@gmail.com>
2015-10-12 19:55:46 -0500

    only show play again button after game has ended. disabled clicks on board after winner has been found.

--- js/main.js
+++ js/main.js
@@ -1,67 +1,75 @@
 var clickCounter = 0;
 var scoreX = 0;
 var scoreO = 0;
+var winnerFound = false;
 
-  function isEven(num){
-    if (num === 0){
-        return true;
-      } else if(num === 1) {
-        return false;
-      } else if(num < 0) {
-      	return isEven(num + 2);
-      }
-    	else {
-        return isEven(num - 2);
-      }
+function isEven(num){
+  if (num === 0){
+      return true;
+    } else if(num === 1) {
+      return false;
+    } else if(num < 0) {
+    	return isEven(num + 2);
+    }
+  	else {
+      return isEven(num - 2);
+    }
   }
 
 function onClick(id){
-  clickCounter++;
-  var x = isEven(clickCounter); //x knows wether the number of clicks is even or odd and tells the dom to "draw and X or O"
-  if($('#'+id).attr("src") != "img/blank.png") {
-    alert("I'm sorry that square is already taken!");
-    clickCounter--;
+  if(winnerFound == true){
+    return;
+  } else {
+    clickCounter++;
+    var x = isEven(clickCounter); //x knows wether the number of clicks is even or odd and tells the dom to "draw and X or O"
+    if($('#'+id).attr("src") != "img/blank.png") {
+      alert("I'm sorry that square is already taken!");
+      clickCounter--;
+    }
+    else if(x == true){
+      $('#'+id).attr("src", "img/O.png");
+    }
+    else if(x == false){
+      $('#'+id).attr("src", "img/X.png");
+    }
+    else {
+      console.log("ERROR!");
+      clickCounter--;
+    }
+    findWinner();
   }
-  else if(x == true){
-    $('#'+id).attr("src", "img/O.png");
-  }
-  else if(x == false){
-    $('#'+id).attr("src", "img/X.png");
-  }
-  else {
-    console.log("ERROR!");
-    clickCounter--;
-  }
-  findWinner();
 }
+
 
 function clearGame(){
   $('#game tr td img').attr("src", "img/blank.png");
 }
 
 function findWinner(){
-  var state = false;
   var win = [['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3'],['a1','b1','c1'],['a2','b2','c2'],['a3','b3','c3'],['a1','b2','c3'],['a3','b2','c1']];
   for (var i = 0; i < win.length; i++){
     var indexOfWin = win[i];
   if($('#'+(indexOfWin[0])).attr("src") == "img/X.png" &&
      $('#'+(indexOfWin[1])).attr("src") == "img/X.png" &&
      $('#'+(indexOfWin[2])).attr("src") == "img/X.png"){
-      state = true;
+      winnerFound = true;
       $('#win-message h1').html("X's have won!!!");
       scoreX++;
       $('#x-score').html(scoreX);
+      $('#play-again').html('<button id="replay" onclick="buttonPress();">Play Again</button>').show();
     }
     else if($('#'+(indexOfWin[0])).attr("src") == "img/O.png" &&
             $('#'+(indexOfWin[1])).attr("src") == "img/O.png" &&
             $('#'+(indexOfWin[2])).attr("src") == "img/O.png"){
-      state = true;
+      winnerFound = true;
       $('#win-message h1').html("O's have won!!!");;
       scoreO++;
       $('#o-score').html(scoreO);
+      $('#play-again').html('<button id="replay" onclick="buttonPress();">Play Again</button>').show();
     }
-    else if(clickCounter == 9 && state == false && i == 7){
-      $('#win-message h1').html("CAT! No winner this time!");
+    else if(clickCounter == 9 && winnerFound == false && i == 7){
+      $('#win-message h1').html("No winner this time! Please play again.");
+      $('#play-again').html('<button id="replay" onclick="buttonPress();">Play Again</button>').show();
     }
   }
 }
@@ -69,5 +77,7 @@ function findWinner(){
 function buttonPress() {
   $('#win-message h1').html('');
   clearGame();
+  winnerFound = false;
   clickCounter = 0;
+  $('#play-again').html('<button id="replay" onclick="buttonPress();">Play Again</button>').hide();
 }
